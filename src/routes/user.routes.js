61b89a0a675f4f41b7014ac48d22c81785c7bbfc
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-03-18 23:00:30 +0530

    Add watch history retrieval functionality for users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router();
+
+import { 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateCoverImage, 
+    updateUserAvatar
+} from "../controllers/user.controllers.js";
 
 // Yahan maine upload.fields isliye likha h kyonki file directly access nhi ho skta isilye usko routes mein likhna padega middleware ki tarah 
 router.route('/register').post(
@@ -18,5 +32,20 @@ router.route('/register').post(
     ]),
     registerUser
 )
+
+router.route('/login').post(loginUser)
+
+//secured Routes
+router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/refresh-Token').post(refreshToken)
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/update-account').patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
